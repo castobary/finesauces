06db4b4d9f406bf020f30bb5c79d149d8f60f948
Castory Munishi <castoryofficial@gmail.com>
2024-03-02 17:55:33 +0100

    filter by category

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 # Create your views here.
 
-def product_list(request):
+def product_list(request,category_slug=None):
     categories = Category.objects.all()
+    requested_category = None
     products = Product.objects.all()
+
+    if category_slug:
+        requested_category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=requested_category)
 
     return render(
         request, 'listings/product_list.html',
        context = {
             'categories': categories,
+            'requested_category': requested_category,
             'products': products
         }
     )
