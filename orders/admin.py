dd9486a2c8cbc9aea6e43ec4d313a12d689e1408
Castory Munishi <castoryofficial@gmail.com>
2024-03-03 14:50:07 +0100

    orders in admin site

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'first_name', 'last_name',
+        'email', 'address', 'postal_code',
+        'city', 'transport', 'created',
+        'status'
+    ]
+    list_filter = [
+        'created', 'updated'
+    ]
+
+    inlines = [OrderItemInline]
