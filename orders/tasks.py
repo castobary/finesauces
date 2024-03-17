26ab7eb62450e872df9eeb8a00e977418a9da58b
Castory Munishi <castoryofficial@gmail.com>
2024-03-17 13:49:47 +0100

    pdf invoices

--- orders/tasks.py
+++ orders/tasks.py
@@ -26,7 +26,7 @@ def order_created(order_id):
   )
 
   # generate pdf
-  html = render_to_string('pdf.html', {'order': order})
+  html = render_to_string('orders/pdf.html', {'order': order})
   out = BytesIO()
   stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
  
