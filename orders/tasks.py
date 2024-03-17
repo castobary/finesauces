4f4704da2c508bf40efcce0fceb17df1419371c1
Castory Munishi <castoryofficial@gmail.com>
2024-03-17 13:42:26 +0100

    send email with pdf invoice

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,6 +1,15 @@
+
 from config.celery import app
 from django.core.mail import send_mail
 from .models import Order
+
+# send invoice by mail
+
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.conf import settings
+from io import BytesIO
+import weasyprint
 
 @app.task
 def order_created(order_id):
@@ -9,13 +18,28 @@ def order_created(order_id):
   message = f'Dear {order.first_name}, \n\n' \
        f'Your order was successfully created.\n' \
        f'Your order ID is {order.id}. Delivery will be in few hours'
-  mail_sent = send_mail(
+  email = EmailMessage(
     subject,
     message,
     'eshop@finesauces.store',
     [order.email]
   )
-  return mail_sent
+
+  # generate pdf
+  html = render_to_string('pdf.html', {'order': order})
+  out = BytesIO()
+  stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+ 
+  weasyprint.HTML(string=html).write_pdf(out,stylesheets=stylesheets)
+
+    # attach PDF file
+  email.attach(f'order_{order.id}.pdf',
+        out.getvalue(),
+        'application/pdf'
+    )
+
+  # send e-mail
+  email.send()
 
 @app.task
 def status_change_notification(order_id):
