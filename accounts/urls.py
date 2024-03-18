b6f53c391eac1d411b74d9414c01c0d713f51cf8
Castory Munishi <castoryofficial@gmail.com>
2024-03-18 16:05:57 +0100

    password reset works

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
      path('login/', views.user_login, name='login'),
      path('register/', views.register, name='register'),
      path('logout/', views.UserLogoutView.as_view(), name='logout'),
-     path('password_change/', auth_views.PasswordChangeView.as_view( success_url=reverse_lazy('accounts:password_change_done')),name='password_change'),
+     path('password_change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('accounts:password_change_done')),name='password_change'),
      path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+     path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')),name='password_reset'),
+     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+     path('password_reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')),name='password_reset_confirm'),
+     path('password_reset/complete/',auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
